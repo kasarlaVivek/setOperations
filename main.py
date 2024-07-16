398d61d644e7f5d186dd82a6385c4d6a3ce8d18a
kasarla sai vivek <vivekkasarla781@gmail.com>
2024-07-16 09:28:25 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+my_set1={1,2,3,4,5,6}
+my_set2={8,9,10}
+#to add elements into the set
+my_set1.add(7)
+#to remove elements from the set
+my_set1.remove(5)
+#to modify the values
+my_set.update(my_set2)
+
+print(my_set)
